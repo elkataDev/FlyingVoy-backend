b706e7f40827b9feed04b7c0666e0565662a64ae
elkataDev <antonioespinola344@gmail.com>
2025-08-26 16:32:31 +0200

    Inicialización Proyecto

---
+++ src/main/java/com/FlyingVoy/FlyingVoy/FlyingVoyApplication.java
@@ -0,0 +1,13 @@
+package com.FlyingVoy.FlyingVoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingVoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyingVoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FlyingVoy/FlyingVoy/FlyingVoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlyingVoy.FlyingVoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyingVoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
